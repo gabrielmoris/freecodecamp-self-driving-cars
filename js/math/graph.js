d129fe1e888d4847a1879bcfcb4dc87e38333224
gabriel <gabrielcmoris@gmail.com>
2024-01-17 22:17:54 +0100

    add class Point and Segment

--- js/math/graph.js
+++ js/math/graph.js
@@ -3,4 +3,14 @@ class Graph {
     this.points = points;
     this.segments = segments;
   }
+
+  draw(ctx) {
+    for (const seg of this.segments) {
+      seg.draw(ctx);
+    }
+
+    for (const point of this.points) {
+      point.draw(ctx);
+    }
+  }
 }
