502627e4e2c01c948e7d3f4c054e37058aef46a4
gabriel <gabrielcmoris@gmail.com>
2024-01-18 20:45:27 +0100

    add points and segments

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,38 @@ class Graph {
     this.segments = segments;
   }
 
+  addPoint(point) {
+    this.points.push(point);
+  }
+
+  containsPoint(point) {
+    return this.points.find((p) => p.equalPoints(point));
+  }
+
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+
+  addSegment(segment) {
+    this.segments.push(segment);
+  }
+
+  containsSegment(segment) {
+    return this.segments.find((s) => s.equalSegments(segment));
+  }
+
+  tryAddSegment(segment) {
+    if (!this.containsSegment(segment) && !segment.p1.equalPoints(segment.p2)) {
+      this.addSegment(segment);
+      return true;
+    }
+    return false;
+  }
+
   draw(ctx) {
     for (const seg of this.segments) {
       seg.draw(ctx);
--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -4,6 +4,18 @@ class Segment {
     this.p2 = p2;
   }
 
+  equalSegments(segment) {
+    // return (
+    //   (this.p1.equalPoints(segment.p1) && this.p2.equalPoints(segment.p2)) || (this.p1.equalPoints(segment.p2) && this.p2.equalPoints(segment.p1))
+    // );
+    // with this helper function is more elegant
+    return this.includes(segment.p1) && this.includes(segment.p2);
+  }
+
+  includes(point) {
+    return this.p1.equalPoints(point) || this.p2.equalPoints(point);
+  }
+
   draw(ctx, width = 2, color = "black") {
     ctx.beginPath();
     ctx.lineWidth = width;
