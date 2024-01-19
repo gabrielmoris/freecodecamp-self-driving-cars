61726ea7afca58e01b6cb5c5dcfdb5adbe66820e
gabriel <gabrielcmoris@gmail.com>
2024-01-19 22:44:58 +0100

    implement functions to drag points, hover points, remove points and add segments

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -5,20 +5,69 @@ class GraphEditor {
     this.ctx = this.canvas.getContext("2d");
 
     this.selected = null;
+    this.hovered = null;
+    this.dragging = false;
 
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (e) => {
-      const mouse = new Point(e.offsetX, e.offsetY);
-      this.graph.addPoint(mouse);
-      this.selected = mouse;
+      //right click
+      if (e.button == 2) {
+        if (this.hovered) {
+          this.#removePoint(this.hovered);
+        } else {
+          this.selected = null;
+        }
+      }
+      //left click
+      if (e.button == 0) {
+        const mouse = new Point(e.offsetX, e.offsetY);
+
+        if (this.hovered) {
+          if (this.selected) {
+            this.graph.tryAddSegment(new Segment(this.selected, this.hovered));
+          }
+          this.selected = this.hovered;
+          this.dragging = true;
+          return;
+        }
+        this.graph.addPoint(mouse);
+        if (this.selected) {
+          this.graph.tryAddSegment(new Segment(this.selected, mouse));
+        }
+        this.selected = mouse;
+        this.hovered = mouse;
+      }
     });
+
+    this.canvas.addEventListener("mousemove", (e) => {
+      const mouse = new Point(e.offsetX, e.offsetY);
+      this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+      if (this.dragging == true) {
+        this.selected.x = mouse.x;
+        this.selected.y = mouse.y;
+      }
+    });
+
+    this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.canvas.addEventListener("mouseup", () => (this.dragging = false));
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point);
+    this.hovered = null;
+    if (this.selected == point) {
+      this.selected = null;
+    }
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
+    }
     if (this.selected) {
       this.selected.draw(this.ctx, { outline: true });
     }
