80771bc7a749b9cf485996085817d7b48f9191ed
gabriel <gabrielcmoris@gmail.com>
2024-01-23 19:21:16 +0100

    start creating the vectors to make the buildings using envelopes of this vectors

--- js/world.js
+++ js/world.js
@@ -1,11 +1,22 @@
 class World {
-  constructor(graph, roadWidth = 100, roadRoundness = 10) {
+  constructor(
+    graph,
+    roadWidth = 100,
+    roadRoundness = 10,
+    buildingWidth = 150,
+    buildingMinLength = 150,
+    spacing = 50
+  ) {
     this.graph = graph;
     this.roadWidth = roadWidth;
     this.roadRoundness = roadRoundness;
+    this.buildingWidth = buildingWidth;
+    this.buildingMinLength = buildingMinLength;
+    this.spacing = spacing;
 
     this.envelopes = [];
     this.roadBorders = [];
+    this.buildings = [];
 
     this.generate();
   }
@@ -13,10 +24,61 @@ class World {
   generate() {
     this.envelopes.length = 0;
     for (const segment of this.graph.segments) {
-      this.envelopes.push(new Envelope(segment, this.roadWidth, this.roadRoundness));
+      this.envelopes.push(
+        new Envelope(segment, this.roadWidth, this.roadRoundness)
+      );
     }
 
     this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
+    this.buildings = this.#generateBuildings();
+  }
+
+  #generateBuildings() {
+    const tmpEnvelopes = [];
+    for (const seg of this.graph.segments) {
+      tmpEnvelopes.push(
+        new Envelope(
+          seg,
+          this.roadWidth + this.buildingWidth + this.spacing * 2,
+          this.roadRoundness
+        )
+      );
+    }
+
+    const guides = Polygon.union(tmpEnvelopes.map((e) => e.poly));
+
+    for (let i = 0; i < guides.length; i++) {
+      const seg = guides[i];
+      if (seg.length() < this.buildingMinLength) {
+        guides.splice(i, 1);
+        i--;
+      }
+    }
+
+    const supports = [];
+
+    for (let seg of guides) {
+      const len = seg.length() + this.spacing;
+
+      const buildingCount = Math.floor(
+        len / (this.buildingMinLength + this.spacing)
+      );
+
+      const buildingLength = len / buildingCount - this.spacing;
+      const dir = seg.directionVector();
+
+      let q1 = seg.p1;
+      let q2 = add(q1, scale(dir, buildingLength));
+      supports.push(new Segment(q1, q2));
+
+      for (let i = 2; i <= buildingCount; i++) {
+        q1 = add(q2, scale(dir, this.spacing));
+        q2 = add(q1, scale(dir, buildingLength));
+        supports.push(new Segment(q1, q2));
+      }
+    }
+
+    return supports;
   }
 
   draw(ctx) {
@@ -30,6 +92,9 @@ class World {
 
     for (const seg of this.roadBorders) {
       seg.draw(ctx, { color: "white", width: 4 });
+    }
+    for (const bld of this.buildings) {
+      bld.draw(ctx);
     }
   }
 }
