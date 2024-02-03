0da92ba005d5e8d795d8221eda5773061e268ba6
gabriel <gabrielcmoris@gmail.com>
2024-02-03 16:56:03 +0100

    finish crossing and style it

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -9,13 +9,17 @@ class Crossing {
 
     this.poly = new Envelope(this.support, width, 0).poly;
 
-    this.border = this.poly.segments[2];
+    this.borders = [this.poly.segments[0], this.poly.segments[2]];
   }
 
   draw(ctx) {
     const perp = perpendicular(this.directionVector);
     const line = new Segment(add(this.center, scale(perp, this.width / 2)), add(this.center, scale(perp, -this.width / 2)));
 
-    line.draw(ctx);
+    line.draw(ctx, {
+      width: this.height,
+      color: "white",
+      dash: [11, 11],
+    });
   }
 }
